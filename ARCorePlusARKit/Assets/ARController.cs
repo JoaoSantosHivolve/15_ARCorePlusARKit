0b144eb7729a240c5ee9b13d91613e44821cad25
JoaoLucas <joao.santos@hivolve.com>
2020-02-06 15:24:49 +0000

    Dual tecnology setup

---
+++ ARCorePlusARKit/Assets/ARController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ArTechnology
+{
+    ArCore, ArKit
+}
+
+public class ARController : MonoBehaviour
+{
+    public GameObject arCore;
+    public GameObject arKit;
+
+    public ArTechnology technology;
+
+    public void Awake()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            technology = ArTechnology.ArCore;
+        }
+        else if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            technology = ArTechnology.ArKit;
+        }
+
+        arCore.SetActive(technology == ArTechnology.ArCore);
+        arKit.SetActive(technology == ArTechnology.ArKit);
+    }
+}
